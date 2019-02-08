9378bdcc1d27b49c40d16ff587a8aebab2ae4549
Xavier Lau <c.estlavie@icloud.com>
2019-02-09 06:00:47 +0800

    fake-useragent

---
+++ src/browser.rs
@@ -0,0 +1,167 @@
+// --- std ---
+use std::collections::{
+    HashSet,
+    hash_set::Iter,
+};
+
+const BROWSERS: [&'static str; 117] = [
+    "ABrowse", "Acoo+Browser", "America+Online+Browser", "AmigaVoyager", "AOL", "Arora", "Avant+Browser", "Beonex", "BonEcho",
+    "Browzar", "Camino", "Charon", "Cheshire", "Chimera", "Chrome", "ChromePlus", "Classilla", "CometBird", "Comodo_Dragon",
+    "Conkeror", "Crazy+Browser", "Cyberdog", "Deepnet+Explorer", "DeskBrowse", "Dillo", "Dooble", "Edge", "Element+Browser",
+    "Elinks", "Enigma+Browser", "EnigmaFox", "Epiphany", "Escape", "Firebird", "Firefox", "Fireweb+Navigator", "Flock", "Fluid",
+    "Galaxy", "Galeon", "GranParadiso", "GreenBrowser", "Hana", "HotJava", "IBM+WebExplorer", "IBrowse", "iCab", "Iceape",
+    "IceCat", "Iceweasel", "iNet+Browser", "Internet+Explorer", "iRider", "Iron", "K-Meleon", "K-Ninja", "Kapiko", "Kazehakase",
+    "Kindle+Browser", "KKman", "KMLite", "Konqueror", "LeechCraft", "Links", "Lobo", "lolifox", "Lorentz", "Lunascape", "Lynx",
+    "Madfox", "Maxthon", "Midori", "Minefield", "Mozilla", "myibrow", "MyIE2", "Namoroka", "Navscape", "NCSA_Mosaic", "NetNewsWire",
+    "NetPositive", "Netscape", "NetSurf", "OmniWeb", "Opera", "Orca", "Oregano", "osb-browser", "Palemoon", "Phoenix", "Pogo",
+    "Prism", "QtWeb+Internet+Browser", "Rekonq", "retawq", "RockMelt", "Safari", "SeaMonkey", "Shiira", "Shiretoko", "Sleipnir",
+    "SlimBrowser", "Stainless", "Sundance", "Sunrise", "surf", "Sylera", "Tencent+Traveler", "TenFourFox", "theWorld+Browser",
+    "uzbl", "Vimprobable", "Vonkeror", "w3m", "WeltweitimnetzBrowser", "WorldWideWeb", "Wyzo"
+];
+
+pub struct Browsers<'a>(HashSet<&'a str>);
+
+macro_rules! set_unset {
+    ($self_:ident, $setter:ident, $unsetter:ident, $name:expr) => {
+        pub fn $setter(mut $self_) -> Self {
+            $self_.0.insert($name);
+            $self_
+        }
+
+        pub fn $unsetter(mut $self_) -> Self {
+            $self_.0.remove($name);
+            $self_
+        }
+    };
+}
+
+impl<'a> Browsers<'a> {
+    pub fn new() -> Browsers<'a> { Browsers(HashSet::new()) }
+
+    pub fn iter(&self) -> Iter<&str> { self.0.iter() }
+
+    set_unset!(self, set_a_browser, unset_a_browser, "ABrowse");
+    set_unset!(self, set_acoo_browser, unset_acoo_browser, "Acoo+Browser");
+    set_unset!(self, set_america_online_browser, unset_america_online_browser, "America+Online+Browser");
+    set_unset!(self, set_amiga_voyager, unset_amiga_voyager, "AmigaVoyager");
+    set_unset!(self, set_aol, unset_aol, "AOL");
+    set_unset!(self, set_arora, unset_arora, "Arora");
+    set_unset!(self, set_avant_browser, unset_avant_browser, "Avant+Browser");
+    set_unset!(self, set_beonex, unset_beonex, "Beonex");
+    set_unset!(self, set_bon_echo, unset_bon_echo, "BonEcho");
+    set_unset!(self, set_browzar, unset_browzar, "Browzar");
+    set_unset!(self, set_camino, unset_camino, "Camino");
+    set_unset!(self, set_charon, unset_charon, "Charon");
+    set_unset!(self, set_cheshire, unset_cheshire, "Cheshire");
+    set_unset!(self, set_chimera, unset_chimera, "Chimera");
+    set_unset!(self, set_chrome, unset_chrome, "Chrome");
+    set_unset!(self, set_chrome_plus, unset_chrome_plus, "ChromePlus");
+    set_unset!(self, set_classilla, unset_classilla, "Classilla");
+    set_unset!(self, set_comet_bird, unset_comet_bird, "CometBird");
+    set_unset!(self, set_comodo_dragon, unset_comodo_dragon, "Comodo_Dragon");
+    set_unset!(self, set_conkeror, unset_conkeror, "Conkeror");
+    set_unset!(self, set_crazy_browser, unset_crazy_browser, "Crazy+Browser");
+    set_unset!(self, set_cyberdog, unset_cyberdog, "Cyberdog");
+    set_unset!(self, set_deepnet_explorer, unset_deepnet_explorer, "Deepnet+Explorer");
+    set_unset!(self, set_desk_browse, unset_desk_browse, "DeskBrowse");
+    set_unset!(self, set_dillo, unset_dillo, "Dillo");
+    set_unset!(self, set_dooble, unset_dooble, "Dooble");
+    set_unset!(self, set_edge, unset_edge, "Edge");
+    set_unset!(self, set_element_browser, unset_element_browser, "Element+Browser");
+    set_unset!(self, set_elinks, unset_elinks, "Elinks");
+    set_unset!(self, set_enigma_browser, unset_enigma_browser, "Enigma+Browser");
+    set_unset!(self, set_enigma_fox, unset_enigma_fox, "EnigmaFox");
+    set_unset!(self, set_epiphany, unset_epiphany, "Epiphany");
+    set_unset!(self, set_escape, unset_escape, "Escape");
+    set_unset!(self, set_firebird, unset_firebird, "Firebird");
+    set_unset!(self, set_firefox, unset_firefox, "Firefox");
+    set_unset!(self, set_fireweb_navigator, unset_fireweb_navigator, "Fireweb+Navigator");
+    set_unset!(self, set_flock, unset_flock, "Flock");
+    set_unset!(self, set_fluid, unset_fluid, "Fluid");
+    set_unset!(self, set_galaxy, unset_galaxy, "Galaxy");
+    set_unset!(self, set_galeon, unset_galeon, "Galeon");
+    set_unset!(self, set_gran_paradiso, unset_gran_paradiso, "GranParadiso");
+    set_unset!(self, set_green_browser, unset_green_browser, "GreenBrowser");
+    set_unset!(self, set_hana, unset_hana, "Hana");
+    set_unset!(self, set_hot_java, unset_hot_java, "HotJava");
+    set_unset!(self, set_ibm_web_explorer, unset_ibm_web_explorer, "IBM+WebExplorer");
+    set_unset!(self, set_i_browse, unset_i_browse, "IBrowse");
+    set_unset!(self, set_i_cab, unset_i_cab, "iCab");
+    set_unset!(self, set_iceape, unset_iceape, "Iceape");
+    set_unset!(self, set_ice_cat, unset_ice_cat, "IceCat");
+    set_unset!(self, set_iceweasel, unset_iceweasel, "Iceweasel");
+    set_unset!(self, set_i_net_browser, unset_i_net_browser, "iNet+Browser");
+    set_unset!(self, set_internet_explorer, unset_internet_explorer, "Internet+Explorer");
+    set_unset!(self, set_i_rider, unset_i_rider, "iRider");
+    set_unset!(self, set_iron, unset_iron, "Iron");
+    set_unset!(self, set_k_meleon, unset_k_meleon, "K-Meleon");
+    set_unset!(self, set_k_ninja, unset_k_ninja, "K-Ninja");
+    set_unset!(self, set_kapiko, unset_kapiko, "Kapiko");
+    set_unset!(self, set_kazehakase, unset_kazehakase, "Kazehakase");
+    set_unset!(self, set_kindle_browser, unset_kindle_browser, "Kindle+Browser");
+    set_unset!(self, set_kk_man, unset_kk_man, "KKman");
+    set_unset!(self, set_km_lite, unset_km_lite, "KMLite");
+    set_unset!(self, set_kongueror, unset_kongueror, "Konqueror");
+    set_unset!(self, set_leech_craft, unset_leech_craft, "LeechCraft");
+    set_unset!(self, set_links, unset_links, "Links");
+    set_unset!(self, set_lobo, unset_lobo, "Lobo");
+    set_unset!(self, set_lolifox, unset_lolifox, "lolifox");
+    set_unset!(self, set_lorentz, unset_lorentz, "Lorentz");
+    set_unset!(self, set_lunascape, unset_lunascape, "Lunascape");
+    set_unset!(self, set_lynx, unset_lynx, "Lynx");
+    set_unset!(self, set_madfox, unset_madfox, "Madfox");
+    set_unset!(self, set_maxthon, unset_maxthon, "Maxthon");
+    set_unset!(self, set_midori, unset_midori, "Midori");
+    set_unset!(self, set_minefield, unset_minefield, "Minefield");
+    set_unset!(self, set_mozilla, unset_mozilla, "Mozilla");
+    set_unset!(self, set_myibrow, unset_myibrow, "myibrow");
+    set_unset!(self, set_my_ie2, unset_my_ie2, "MyIE2");
+    set_unset!(self, set_namoroka, unset_namoroka, "Namoroka");
+    set_unset!(self, set_navscape, unset_navscape, "Navscape");
+    set_unset!(self, set_ncsa_mosaic, unset_ncsa_mosaic, "NCSA_Mosaic");
+    set_unset!(self, set_net_news_wire, unset_net_news_wire, "NetNewsWire");
+    set_unset!(self, set_net_positive, unset_net_positive, "NetPositive");
+    set_unset!(self, set_net_scape, unset_net_scape, "Netscape");
+    set_unset!(self, set_net_surf, unset_net_surf, "NetSurf");
+    set_unset!(self, set_omni_web, unset_omni_web, "OmniWeb");
+    set_unset!(self, set_opera, unset_opera, "Opera");
+    set_unset!(self, set_orca, unset_orca, "Orca");
+    set_unset!(self, set_oregano, unset_oregano, "Oregano");
+    set_unset!(self, set_osb_browser, unset_osb_browser, "osb-browser");
+    set_unset!(self, set_palemoon, unset_palemoon, "Palemoon");
+    set_unset!(self, set_phoenix, unset_phoenix, "Phoenix");
+    set_unset!(self, set_pogo, unset_pogo, "Pogo");
+    set_unset!(self, set_prism, unset_prism, "Prism");
+    set_unset!(self, set_qt_web_internet_browser, unset_qt_web_internet_browser, "QtWeb+Internet+Browser");
+    set_unset!(self, set_rekona, unset_rekona, "Rekonq");
+    set_unset!(self, set_retawq, unset_retawq, "retawq");
+    set_unset!(self, set_rock_melt, unset_rock_melt, "RockMelt");
+    set_unset!(self, set_safari, unset_safari, "Safari");
+    set_unset!(self, set_sea_monkey, unset_sea_monkey, "SeaMonkey");
+    set_unset!(self, set_shiira, unset_shiira, "Shiira");
+    set_unset!(self, set_shiretoko, unset_shiretoko, "Shiretoko");
+    set_unset!(self, set_sleipnir, unset_sleipnir, "Sleipnir");
+    set_unset!(self, set_slim_browser, unset_slim_browser, "SlimBrowser");
+    set_unset!(self, set_stainless, unset_stainless, "Stainless");
+    set_unset!(self, set_sundance, unset_sundance, "Sundance");
+    set_unset!(self, set_surf, unset_surf, "surf");
+    set_unset!(self, set_sylera, unset_sylera, "Sylera");
+    set_unset!(self, set_tecent_traveler, unset_tecent_traveler, "Tencent+Traveler");
+    set_unset!(self, set_ten_four_fox, unset_ten_four_fox, "TenFourFox");
+    set_unset!(self, set_the_world_browser, unset_the_world_browser, "theWorld+Browser");
+    set_unset!(self, set_uzbl, unset_uzbl, "uzbl");
+    set_unset!(self, set_vimprobable, unset_vimprobable, "Vimprobable");
+    set_unset!(self, set_vonkeror, unset_vonkeror, "Vonkeror");
+    set_unset!(self, set_w3m, unset_w3m, "w3m");
+    set_unset!(self, set_weltweitimnetz_browser, unset_weltweitimnetz_browser, "WeltweitimnetzBrowser");
+    set_unset!(self, set_world_wide_web, unset_world_wide_web, "WorldWideWeb");
+    set_unset!(self, set_wyzo, unset_wyzo, "Wyzo");
+    pub fn set_all(mut self) -> Self {
+        self.0 = BROWSERS.iter().cloned().collect();
+        self
+    }
+    pub fn unset_all(mut self) -> Self {
+        self.0 = HashSet::new();
+        self
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,277 +1,34 @@
 extern crate reqwest;
 extern crate select;
 
-const USER_AGENTS_PAGE_PREFIX: &'static str = "http://www.useragentstring.com/pages/useragentstring.php?name=";
+const API: &'static str = "http://www.useragentstring.com/pages/useragentstring.php?name=";
 
-enum Browser {
-    ABrowse,
-    AcooBrowser,
-    AmericaOnlineBrowser,
-    AmigaVoyager,
-    AOL,
-    Arora,
-    AvantBrowser,
-    Beonex,
-    BonEcho,
-    Browzar,
-    Camino,
-    Charon,
-    Cheshire,
-    Chimera,
-    Chrome,
-    ChromePlus,
-    Classilla,
-    CometBird,
-    ComodoDragon,
-    Conkeror,
-    CrazyBrowser,
-    Cyberdog,
-    DeepnetExplorer,
-    DeskBrowse,
-    Dillo,
-    Dooble,
-    Edge,
-    ElementBrowser,
-    Elinks,
-    EnigmaBrowser,
-    EnigmaFox,
-    Epiphany,
-    Escape,
-    Firebird,
-    Firefox,
-    FirewebNavigator,
-    Flock,
-    Fluid,
-    Galaxy,
-    Galeon,
-    GranParadiso,
-    GreenBrowser,
-    Hana,
-    HotJava,
-    IBMWebExplorer,
-    IBrowse,
-    ICab,
-    Iceape,
-    IceCat,
-    Iceweasel,
-    INetBrowser,
-    InternetExplorer,
-    IRider,
-    Iron,
-    KMeleon,
-    KNinja,
-    Kapiko,
-    Kazehakase,
-    KindleBrowser,
-    KKman,
-    KMLite,
-    Konqueror,
-    LeechCraft,
-    Links,
-    Lobo,
-    Lolifox,
-    Lorentz,
-    Lunascape,
-    Lynx,
-    Madfox,
-    Maxthon,
-    Midori,
-    Minefield,
-    Mozilla,
-    Myibrow,
-    MyIE2,
-    Namoroka,
-    Navscape,
-    NCSAMosaic,
-    NetNewsWire,
-    NetPositive,
-    Netscape,
-    NetSurf,
-    OmniWeb,
-    Opera,
-    Orca,
-    Oregano,
-    OsbBrowser,
-    Palemoon,
-    Phoenix,
-    Pogo,
-    Prism,
-    QtWebInternetBrowser,
-    Rekonq,
-    Retawq,
-    RockMelt,
-    Safari,
-    SeaMonkey,
-    Shiira,
-    Shiretoko,
-    Sleipnir,
-    SlimBrowser,
-    Stainless,
-    Sundance,
-    Sunrise,
-    Surf,
-    Sylera,
-    TencentTraveler,
-    TenFourFox,
-    TheWorldBrowser,
-    Uzbl,
-    Vimprobable,
-    Vonkeror,
-    W3M,
-    WeltweitimnetzBrowser,
-    WorldWideWeb,
-    Wyzo,
-}
+mod browser;
 
-impl Browser {
-    fn api(&self) -> String {
-        // --- custom ---
-        use Browser::*;
+// --- custom ---
+use browser::Browsers;
 
-        String::from(USER_AGENTS_PAGE_PREFIX) + match self {
-            ABrowse => "ABrowse",
-            AcooBrowser => "Acoo+Browser",
-            AmericaOnlineBrowser => "America+Online+Browser",
-            AmigaVoyager => "AmigaVoyager",
-            AOL => "AOL",
-            Arora => "Arora",
-            AvantBrowser => "Avant+Browser",
-            Beonex => "Beonex",
-            BonEcho => "BonEcho",
-            Browzar => "Browzar",
-            Camino => "Camino",
-            Charon => "Charon",
-            Cheshire => "Cheshire",
-            Chimera => "Chimera",
-            Chrome => "Chrome",
-            ChromePlus => "ChromePlus",
-            Classilla => "Classilla",
-            CometBird => "CometBird",
-            ComodoDragon => "Comodo_Dragon",
-            Conkeror => "Conkeror",
-            CrazyBrowser => "Crazy+Browser",
-            Cyberdog => "Cyberdog",
-            DeepnetExplorer => "Deepnet+Explorer",
-            DeskBrowse => "DeskBrowse",
-            Dillo => "Dillo",
-            Dooble => "Dooble",
-            Edge => "Edge",
-            ElementBrowser => "Element+Browser",
-            Elinks => "Elinks",
-            EnigmaBrowser => "Enigma+Browser",
-            EnigmaFox => "EnigmaFox",
-            Epiphany => "Epiphany",
-            Escape => "Escape",
-            Firebird => "Firebird",
-            Firefox => "Firefox",
-            FirewebNavigator => "Fireweb+Navigator",
-            Flock => "Flock",
-            Fluid => "Fluid",
-            Galaxy => "Galaxy",
-            Galeon => "Galeon",
-            GranParadiso => "GranParadiso",
-            GreenBrowser => "GreenBrowser",
-            Hana => "Hana",
-            HotJava => "HotJava",
-            IBMWebExplorer => "IBM+WebExplorer",
-            IBrowse => "IBrowse",
-            ICab => "iCab",
-            Iceape => "Iceape",
-            IceCat => "IceCat",
-            Iceweasel => "Iceweasel",
-            INetBrowser => "iNet+Browser",
-            InternetExplorer => "Internet+Explorer",
-            IRider => "iRider",
-            Iron => "Iron",
-            KMeleon => "K-Meleon",
-            KNinja => "K-Ninja",
-            Kapiko => "Kapiko",
-            Kazehakase => "Kazehakase",
-            KindleBrowser => "Kindle+Browser",
-            KKman => "KKman",
-            KMLite => "KMLite",
-            Konqueror => "Konqueror",
-            LeechCraft => "LeechCraft",
-            Links => "Links",
-            Lobo => "Lobo",
-            Lolifox => "lolifox",
-            Lorentz => "Lorentz",
-            Lunascape => "Lunascape",
-            Lynx => "Lynx",
-            Madfox => "Madfox",
-            Maxthon => "Maxthon",
-            Midori => "Midori",
-            Minefield => "Minefield",
-            Mozilla => "Mozilla",
-            Myibrow => "myibrow",
-            MyIE2 => "MyIE2",
-            Namoroka => "Namoroka",
-            Navscape => "Navscape",
-            NCSAMosaic => "NCSA_Mosaic",
-            NetNewsWire => "NetNewsWire",
-            NetPositive => "NetPositive",
-            Netscape => "Netscape",
-            NetSurf => "NetSurf",
-            OmniWeb => "OmniWeb",
-            Opera => "Opera",
-            Orca => "Orca",
-            Oregano => "Oregano",
-            OsbBrowser => "osb-browser",
-            Palemoon => "Palemoon",
-            Phoenix => "Phoenix",
-            Pogo => "Pogo",
-            Prism => "Prism",
-            QtWebInternetBrowser => "QtWeb+Internet+Browser",
-            Rekonq => "Rekonq",
-            Retawq => "retawq",
-            RockMelt => "RockMelt",
-            Safari => "Safari",
-            SeaMonkey => "SeaMonkey",
-            Shiira => "Shiira",
-            Shiretoko => "Shiretoko",
-            Sleipnir => "Sleipnir",
-            SlimBrowser => "SlimBrowser",
-            Stainless => "Stainless",
-            Sundance => "Sundance",
-            Sunrise => "Sunrise",
-            Surf => "surf",
-            Sylera => "Sylera",
-            TencentTraveler => "Tencent+Traveler",
-            TenFourFox => "TenFourFox",
-            TheWorldBrowser => "theWorld+Browser",
-            Uzbl => "uzbl",
-            Vimprobable => "Vimprobable",
-            Vonkeror => "Vonkeror",
-            W3M => "w3m",
-            WeltweitimnetzBrowser => "WeltweitimnetzBrowser",
-            WorldWideWeb => "WorldWideWeb",
-            Wyzo => "Wyzo",
-        }
-    }
-}
-
-enum Kind {
-    Crawlers,
-    Browsers(Option<Browser>),
-    MobileBrowsers,
-    Consoles,
-    OfflineBrowsers,
-    EmailClients,
-    LinkCheckers,
-    EmailCollectors,
-    Validators,
-    FeedReaders,
-    Libraries,
-    CloudPlatforms,
-    Others,
+enum Kind<'a> {
+//    Crawlers,
+    Browsers(Browsers<'a>),
+//    MobileBrowsers,
+//    Consoles,
+//    OfflineBrowsers,
+//    EmailClients,
+//    LinkCheckers,
+//    EmailCollectors,
+//    Validators,
+//    FeedReaders,
+//    Libraries,
+//    CloudPlatforms,
+//    Others,
 }
 
 struct UserAgent<'a> {
     cache: bool,
     path: &'a str,
-    amount: Option<u32>,
     user_agents: Vec<String>,
-    kinds: Option<Vec<Kind>>,
+    kinds: Vec<Kind<'a>>,
 }
 
 impl<'a> UserAgent<'a> {
@@ -279,9 +36,8 @@ impl<'a> UserAgent<'a> {
         UserAgent {
             cache: false,
             path: ".",
-            amount: None,
             user_agents: vec![],
-            kinds: None,
+            kinds: vec![],
         }
     }
 
@@ -304,7 +60,7 @@ impl<'a> UserAgent<'a> {
         }
     }
 
-    fn parse_user_agents_page(html: String) -> Vec<String> {
+    fn parse(html: String) -> Vec<String> {
         // --- external ---
         use select::{
             document::Document,
@@ -320,10 +76,6 @@ impl<'a> UserAgent<'a> {
         }
 
         user_agents
-    }
-
-    fn parse_list_page(html: String) -> Vec<&str> {
-        unimplemented!()
     }
 
     fn cache(mut self, cache: bool) -> Self {
@@ -351,65 +103,57 @@ impl<'a> UserAgent<'a> {
         self
     }
 
-    fn amount(mut self, amount: u32) -> Self {
-        self.amount = Some(amount);
+    fn kind(mut self, kind: Kind<'a>) -> Self {
+        self.kinds = vec![kind];
         self
     }
 
-    fn kind(mut self, kind: Kind) -> Self {
-        self.kinds = Some(vec![kind]);
+    fn kinds(mut self, kinds: Vec<Kind<'a>>) -> Self {
+        self.kinds = kinds;
         self
     }
 
-    fn kinds(mut self, kinds: Vec<Kind>) -> Self {
-        self.kinds = Some(kinds);
-        self
+    fn store(&self) {
+        // --- std ---
+        use std::{
+            fs::File,
+            io::Write,
+        };
+
+        let mut file = File::create(format!("{}/user_agents", self.path.trim_end_matches('/'))).unwrap();
+        file.write_all(self.user_agents.join("\n").as_bytes()).unwrap();
+        file.sync_all().unwrap();
     }
 
     fn fetch(mut self) -> Self {
-        if let Some(kinds) = &self.kinds {
-            for kind in kinds {
-                // --- custom ---
-                use Kind::*;
+        for kind in &self.kinds {
+            // --- custom ---
+            use Kind::*;
 
-                let url = match kind {
-                    Crawlers => unimplemented!(),
-                    Browsers(browser) => if let Some(browser) = browser { browser.api() } else {
+            match kind {
+//                Crawlers => unimplemented!(),
+                Browsers(browsers) => for browser in browsers.iter() {
+                    println!("{}", browser);
+                    let html = UserAgent::get(&format!("{}{}", API, browser));
+                    let user_agents = UserAgent::parse(html);
 
-                    }
-                    MobileBrowsers => unimplemented!(),
-                    Consoles => unimplemented!(),
-                    OfflineBrowsers => unimplemented!(),
-                    EmailClients => unimplemented!(),
-                    LinkCheckers => unimplemented!(),
-                    EmailCollectors => unimplemented!(),
-                    Validators => unimplemented!(),
-                    FeedReaders => unimplemented!(),
-                    Libraries => unimplemented!(),
-                    CloudPlatforms => unimplemented!(),
-                    Others => unimplemented!(),
-                };
-                let html = UserAgent::get(&url);
-                let user_agents = UserAgent::parse_user_agents_page(html);
-
-                self.user_agents.extend_from_slice(&user_agents);
-            }
-        } else {
-            let html = UserAgent::get("http://www.useragentstring.com/pages/useragentstring.php");
-            let user_agent_page = UserAgent::parse_lit_page(&html);
-        }
-
-        if self.cache {
-            // --- std ---
-            use std::{
-                fs::File,
-                io::Write,
+                    self.user_agents.extend_from_slice(&user_agents);
+                }
+//                MobileBrowsers => unimplemented!(),
+//                Consoles => unimplemented!(),
+//                OfflineBrowsers => unimplemented!(),
+//                EmailClients => unimplemented!(),
+//                LinkCheckers => unimplemented!(),
+//                EmailCollectors => unimplemented!(),
+//                Validators => unimplemented!(),
+//                FeedReaders => unimplemented!(),
+//                Libraries => unimplemented!(),
+//                CloudPlatforms => unimplemented!(),
+//                Others => unimplemented!(),
             };
-
-            let mut file = File::create(format!("{}/user_agents", self.path.trim_end_matches('/'))).unwrap();
-            file.write_all(self.user_agents.join("\n").as_bytes()).unwrap();
-            file.sync_all().unwrap();
         }
+
+        if self.cache { self.store(); }
 
         self
     }
@@ -426,9 +170,9 @@ impl<'a> UserAgent<'a> {
 }
 
 fn main() {
-    let mut useragent = UserAgent::new()
+    let useragent = UserAgent::new()
 //        .load_cache("./user_agents");
-        .kind(Kind::Browsers(Some(Browser::Chrome)))
+        .kind(Kind::Browsers(Browsers::new().set_all()))
         .cache(true)
         .fetch();
 
